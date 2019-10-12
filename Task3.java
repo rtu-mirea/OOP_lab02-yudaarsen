e2ae7b7ea4501852c3423d9e1e9cc52bf2495a00
unknown <yudaarsen.ca@gmail.com>
2019-10-12 15:20:41 +0300

    Task3 fixed.

--- Task3.java
+++ Task3.java
@@ -11,33 +11,49 @@ public class Task3 {
 
     public void isDateFormat()
     {
-        Pattern format = Pattern.compile("^\\d{2}\\.\\d{2}\\.\\d{4}$");
-        Matcher matcher = format.matcher(data);
+        Pattern rusFormat = Pattern.compile("^\\d{2}\\.\\d{2}\\.\\d{4}$"); //"^\\d{2}\\.\\d{2}\\.\\d{4}$"
+        Pattern usaFormat = Pattern.compile("^\\d{2}-\\d{2}-\\d{4}$");
+        Matcher matcher = rusFormat.matcher(data);
         if(matcher.find()) {
-            if(Integer.parseInt(data.substring(matcher.start(), matcher.start() + 2)) > 0 &&
-                    Integer.parseInt(data.substring(matcher.start() + 3, matcher.start() + 5)) > 0 &&
-                    Integer.parseInt(data.substring(matcher.start(), matcher.start() + 2)) < 31 &&
-                    Integer.parseInt(data.substring(matcher.start() + 3, matcher.start() + 5)) < 31) {
-                System.out.println("Строка является датой в русском или американском формате.");
-                return;
+            Pattern rusCheck = Pattern.compile("\\d{2}");
+            matcher = rusCheck.matcher(data);
+            if(matcher.find() && Integer.parseInt(matcher.group()) > 0 && Integer.parseInt(matcher.group()) <= 31)
+            {
+                if(matcher.find() && Integer.parseInt(matcher.group()) > 0 && Integer.parseInt(matcher.group()) <= 12)
+                {
+                    rusCheck = Pattern.compile("\\d{4}$");
+                    matcher = rusCheck.matcher(data);
+                    if(matcher.find() && Integer.parseInt(matcher.group()) > 0) {
+                        System.out.println("Строка является датой в русском формате.");
+                        return;
+                    }
+                }
+            }
+        }
+        else
+        {
+            matcher = usaFormat.matcher(data);
+            Pattern usaCheck = Pattern.compile("\\d{2}");
+            matcher = usaCheck.matcher(data);
+            if(matcher.find() && Integer.parseInt(matcher.group()) > 0 && Integer.parseInt(matcher.group()) <= 12)
+            {
+                if(matcher.find() && Integer.parseInt(matcher.group()) > 0 && Integer.parseInt(matcher.group()) <= 31)
+                {
+                    usaCheck = Pattern.compile("\\d{4}$");
+                    matcher = usaCheck.matcher(data);
+                    if(matcher.find() && Integer.parseInt(matcher.group()) > 0) {
+                        System.out.println("Строка является датой в американском формате.");
+                        return;
+                    }
+                }
             }
         }
         System.out.println("Некорректный формат.");
     }
 
     public void changeFormat(String text) {
-        Pattern format = Pattern.compile("\\d{2}\\.\\d{2}\\.\\d{4}");
-        Matcher matcher = format.matcher(text);
-        String hstr;
         String result = text;
-        if(matcher.find())
-        {
-            do{
-                hstr = result.substring(matcher.start() + 3, matcher.start() + 5) + "." +
-                        result.substring(matcher.start(), matcher.start() + 2) + "." + result.substring(matcher.start() + 6, matcher.end());
-                result = result.substring(0, matcher.start()) + hstr + result.substring(matcher.end());
-            }while(matcher.find(matcher.start() + 1));
-        }
+        result = result.replaceAll("(\\d{2})-(\\d{2})-(\\d{4})", "$2\\.$1\\.$3");
         System.out.println(result);
     }
 }
